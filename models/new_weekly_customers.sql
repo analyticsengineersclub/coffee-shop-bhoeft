52d42e283a45263acf36fb63c63aee0baa0b6cba
Ubuntu <ubuntu@ip-172-31-20-7.us-east-2.compute.internal>
2021-10-18 17:21:29 +0000

    dbt 101: replace hardcoded view dependency with ref() DAG sauce

--- models/new_weekly_customers.sql
+++ models/new_weekly_customers.sql
@@ -1,7 +1,7 @@
 SELECT 
-    -- week defined: begins Monday, ends day before next begin date
+    -- signup_week def: begins on a Monday datetime
     date_trunc(first_order_at, WEEK(MONDAY)) AS signup_week,
     COUNT(customer_id) AS new_customers
-FROM `dbt_BrandonHoeft.customers`
+FROM {{ ref('customers') }}
 GROUP BY signup_week
 ORDER BY signup_week
