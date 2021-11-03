8e89df6f61849cba1d6148eaf29476468177753e
Ubuntu <ubuntu@ip-172-31-20-7.us-east-2.compute.internal>
2021-11-03 02:16:26 +0000

    add: source for web_tracking schema and pageviews table

---
+++ models/staging/web_tracking/stg_web_tracking__pageviews.sql
@@ -0,0 +1,20 @@
+with source as(
+
+    select * from {{ source('web_tracking', 'pageviews') }}
+
+)
+
+, renamed as(
+
+    select
+        id as pageview_id,
+        visitor_id,
+        device_type,
+        timestamp as viewed_at,
+        page, 
+        customer_id
+    from source
+
+)
+
+select * from renamed
