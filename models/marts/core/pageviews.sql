81bf36d228b5adabc701f8c158c04fae1590c24e
Ubuntu <ubuntu@ip-172-31-20-7.us-east-2.compute.internal>
2021-11-04 17:50:26 +0000

    exercise 2: sessionization update for pageviews model

--- models/marts/core/pageviews.sql
+++ models/marts/core/pageviews.sql
@@ -51,7 +51,8 @@ with loggedin_pageviews as(
 
 )
 
-, recombined as(
+-- solves AEC Week 4: Exercise 1
+, user_stitching as(
 
     select * from stitched_pageviews
 
@@ -61,5 +62,62 @@ with loggedin_pageviews as(
 
 )
 
-select * 
-from recombined
+/* solves AEC week 4: Exercise 2 (all CTEs here until end)
+   Modeling Decision: include both exercises into a single model */
+, per_visitor_pageview_viewed_at_lag1 as (
+
+select
+    pageview_id,
+    customer_id,
+    visitor_id,
+    device_type,
+    page,
+    viewed_at,
+    lag(viewed_at, 1, null) over(partition by customer_id, visitor_id order by viewed_at asc) viewed_at_lag1
+from user_stitching
+)
+
+
+, per_visitor_pageview_minutes_diff_prior as(
+
+select *,
+    timestamp_diff(viewed_at, viewed_at_lag1, minute) as minutes_diff_from_prior_view
+from per_visitor_pageview_viewed_at_lag1
+
+)
+
+
+, per_visitor_identify_new_session as(
+
+select *,
+    case 
+        when minutes_diff_from_prior_view is null then 1
+        when minutes_diff_from_prior_view <= 30 then 0
+        else 1
+    end as is_new_session
+from per_visitor_pageview_minutes_diff_prior
+
+)
+
+
+, sessionized_pageviews as(
+
+select
+    pageview_id,
+    customer_id,
+    visitor_id,
+    device_type,
+    viewed_at,
+    page,
+    viewed_at_lag1,
+    minutes_diff_from_prior_view,
+    is_new_session,
+    sum(is_new_session) over(partition by customer_id, visitor_id order by viewed_at asc) as session_id
+from per_visitor_identify_new_session
+
+)
+
+select *
+from sessionized_pageviews
+
+
