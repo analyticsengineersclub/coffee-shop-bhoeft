5213427d9e66798422ffa7d9d3823b2d4fe139a3
Ubuntu <ubuntu@ip-172-31-20-7.us-east-2.compute.internal>
2021-11-14 00:28:25 +0000

    added, to help with customer LTV model

---
+++ models/marts/core/dim_dates.sql
@@ -0,0 +1,32 @@
+
+-- alternatively, could use dbt's date_utils package for date_spine()
+-- here, a dimension dates (different date format varieties) is built from unique order and pageview dates
+
+with deduplicated_days as(
+
+    select 
+        distinct date_trunc(ordered_at, day) as date_day 
+    from {{ ref('stg_coffee_shop__orders') }}
+
+    union distinct 
+
+    select 
+        distinct date_trunc(viewed_at, day) as date_day 
+    from {{ ref('stg_web_tracking__pageviews') }}
+
+)
+
+ , dim_dates as(
+
+    select 
+        date_day,
+        date_trunc(date_day, week) as ordered_at_week,
+        date_trunc(date_day, month) as ordered_at_month,
+        date_trunc(date_day, year) as ordered_at_year
+    from deduplicated_days
+
+)
+
+select *
+from dim_dates
+order by date_day
