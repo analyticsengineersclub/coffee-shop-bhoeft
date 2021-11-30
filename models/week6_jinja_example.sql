2ed3a4c1db1aec1b2d6b411ac81b799d0c47cd97
Ubuntu <ubuntu@ip-172-31-20-7.us-east-2.compute.internal>
2021-11-30 03:58:53 +0000

    update: add jinja for loop and if stmt

--- models/week6_jinja_example.sql
+++ models/week6_jinja_example.sql
@@ -1,7 +1,24 @@
-select
-  ordered_at_month as date_month,
-  sum(case when product_category = 'coffee beans' then product_price end) as coffee_beans_amount,
-  sum(case when product_category = 'merch' then product_price end) as merch_amount,
-  sum(case when product_category = 'brewing supplies' then product_price end) as brewing_supplies_amount
-from {{ ref('sales_breakdown') }}
-group by 1
+{% set product_categories = ['coffee beans', 'merch', 'brewing supplies'] %}
+
+with sales_breakdown as (
+
+  select * from {{ ref('sales_breakdown') }}
+
+)
+
+, product_category_sales_by_month as (
+
+  select
+    ordered_at_month as date_month,
+    {% for category in product_categories %}
+    sum(case when product_category = '{{ category }}' then product_price end) as {{ category | replace(' ', '_') }}_amount
+    {% if not loop.last %}
+    ,  -- add a comma after all but last new column iteration
+    {% endif %}
+    {% endfor %}
+  from sales_breakdown
+  group by 1
+
+)
+
+select * from product_category_sales_by_month
